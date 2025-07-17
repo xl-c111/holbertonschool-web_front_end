d22dcb90320b335004fd0f940cb08394e47b1453
xl-c111 <10430@holbertonstudents.com>
2025-07-17 14:58:09 +1000

    Enhance create_file.sh to add CSS link to HTML

--- CSS_advanced/styles/create_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename=$(ls -1 | grep ^'[0-9]' | sort -V | tail -n 1)
-prevNum=${filename%-style.css}
-newNum=$((prevNum + 1))
-cp $filename "${newNum}-style.css"
-code "${newNum}-style.css"
